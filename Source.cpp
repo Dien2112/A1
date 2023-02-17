8c2e883a27a2ba16ef7cf9b8a1ef1afad33313cc
Dien2112 <116195602+Dien2112@users.noreply.github.com>
2023-02-17 20:45:51 +0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-void Sapxepmang(int a[], int m, int b[], int n, int KQ[], int& p) {
-	p = 0; int i = 0, j = 0;
-	do {
-		if (i == m) KQ[p] = b[j++]; else
-			if (j == n) KQ[p] = a[i++]; else
-				if (a[i] > b[j]) KQ[p] = b[j++]; else KQ[p] = a[i++];
-		p++;
-	} while (p < m + n);
+void Queen(int a[][20], int m, int n) {
+	int dem = 0;
+	for (int i = 0; i < m; i++) {
+		int jMax = 0; bool ok = true;
+		for (int j = 1; j < n; j++) if (a[i][j] > a[i][jMax]) jMax = j; 
+		for (int j = 0; j < n; j++) if (j != jMax && a[i][j] == a[i][jMax]) ok = false;
+		for (int k = 0; k < m; k++) if (a[k][jMax] > a[i][jMax]) ok = false;
 
+		for (int k = 1; i+k<m && jMax+k<n; k++) if (a[i+k][jMax+k] >= a[i][jMax]) ok = false;
+		for (int k = 1; i + k < m && jMax - k >=0; k++) if (a[i + k][jMax - k] >= a[i][jMax]) ok = false;
+		for (int k = 1; i - k >=0 && jMax + k < n; k++) if (a[i - k][jMax + k] >= a[i][jMax]) ok = false;
+		for (int k = 1; i - k >= 0 && jMax - k >= 0; k++) if (a[i - k][jMax - k] >= a[i][jMax]) ok = false;
+		if (ok) {
+			dem++; cout << i << " " << jMax << endl;
+		}
+	}
+	cout << dem;
 }
 int main() {
-	int a[] = { 3,5,12,67 };
-	int b[] = { 5,23,34,45,56,67 };
-	int KQ[20], p;
-	Sapxepmang(a, 4, b, 6, KQ, p);
-	for (int i = 0; i < p; i++) cout << KQ[i] << " ";
+	int a[3][20];
+	fstream f; f.open("Text.txt", ios::in);
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++) 
+		f >> a[i][j];
+	Queen(a, 3, 3);
 	return 0;
 }
